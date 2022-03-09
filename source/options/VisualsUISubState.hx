e482987b41d555a091891aca53a353b6df4adc82
piteous <piteousemail@gmail.com>
2022-03-08 22:54:16 -0500

    first commit

---
+++ source/options/VisualsUISubState.hx
@@ -0,0 +1,133 @@
+package options; 
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+
+using StringTools;
+
+class VisualsUISubState extends BaseOptionsMenu
+{
+	public function new() //if you are trying to add a new option, you have to go to clientprefs.hx too to add the option there or you're going to fuck up and the checkmark wont appear for your option.
+	{
+		title = 'Visuals and UI';
+		rpcTitle = 'Visuals & UI Settings Menu'; //for Discord Rich Presence
+
+		var option:Option = new Option('Note Splashes',
+			"If unchecked, hitting \"Sick!\" notes won't show particles.",
+			'noteSplashes',
+			'bool',
+			true);
+		addOption(option);
+
+		var option:Option = new Option('Hide HUD',
+			'If checked, hides most HUD elements excluding the health bar.',
+			'hideHud',
+			'bool',
+			false);
+		addOption(option);
+
+		var option:Option = new Option('Hide Health Bar',
+			'If checked, hides the health bar.',
+			'hideHealthBar',
+			'bool',
+			false);
+		addOption(option);
+		
+		var option:Option = new Option('Time Bar:',
+			"What should the Time Bar display?",
+			'timeBarType',
+			'string',
+			'Time Left',
+			['Time Left', 'Time Elapsed', 'Song Name', 'Disabled']);
+		addOption(option);
+
+		var option:Option = new Option('Flashing Lights',
+			"Uncheck this if you're sensitive to flashing lights!",
+			'flashing',
+			'bool',
+			true);
+		addOption(option);
+
+		var option:Option = new Option('Camera Zooms',
+			"If unchecked, the camera won't zoom in on a beat hit.",
+			'camZooms',
+			'bool',
+			true);
+		addOption(option);
+
+		var option:Option = new Option('Camera Effects', // the new options simply create new boolean variables which can be used in lua scripts, they dont change any source code shit
+			"If unchecked, the camera won't shake or move around for the most part.", // made specifically to be used in modcharts and for people who do not like the massive camera shaking
+			'camEffects',
+			'bool',
+			true);
+		addOption(option);
+
+		var option:Option = new Option('Camera Follows Notes', // the new options simply create new boolean variables which can be used in lua scripts, they dont change any source code shit
+			"If unchecked, the camera won't follow notes hit by the player or enemy.", // it looks wrong if it doesn't follow the camera so this is a separate option
+			'camFollowsNotes',
+			'bool',
+			true);
+		addOption(option);
+
+		var option:Option = new Option('Score Text Zoom on Hit',
+			"If unchecked, disables the Score text zooming\neverytime you hit a note.",
+			'scoreZoom',
+			'bool',
+			true);
+		addOption(option);
+
+		var option:Option = new Option('Health Bar Transparency',
+			'How much transparent should the health bar and icons be.',
+			'healthBarAlpha',
+			'percent',
+			1);
+		option.scrollSpeed = 1.6;
+		option.minValue = 0.0;
+		option.maxValue = 1;
+		option.changeValue = 0.1;
+		option.decimals = 1;
+		addOption(option);
+		
+		#if !mobile
+		var option:Option = new Option('FPS Counter',
+			'If unchecked, hides FPS Counter.',
+			'showFPS',
+			'bool',
+			true);
+		addOption(option);
+		option.onChange = onChangeFPSCounter;
+		#end
+
+		super();
+	}
+
+	#if !mobile
+	function onChangeFPSCounter()
+	{
+		if(Main.fpsVar != null)
+			Main.fpsVar.visible = ClientPrefs.showFPS;
+	}
+	#end
+}
